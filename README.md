2b31b3abca683f447721e1dfdde62a96839c2664
kckuei <kevinkuei@gmail.com>
2022-09-11 09:25:27 -0700

    Add input validation

--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 # odin-mastermind
-Toy [mastermind](https://en.wikipedia.org/wiki/Mastermind_(board_game)) game
+Toy mastermind game implemented in console with `ruby`.
 
-* [Example](https://www.youtube.com/watch?v=dMHxyulGrEk)
-* [Strategy](https://puzzling.stackexchange.com/questions/546/clever-ways-to-solve-mastermind)
+About the Game
+* [Game wiki]((https://en.wikipedia.org/wiki/Mastermind_(board_game)))
+* [Primer](https://www.youtube.com/watch?v=dMHxyulGrEk)
+* [AI Strategy](https://puzzling.stackexchange.com/questions/546/clever-ways-to-solve-mastermind)
+
+
+ decide number of turns (e.g. 12)?
+ decide codebreaker vs codemaker?
+ decide how many games? (1 game means computer and player swap roles)
+ decide if blanks or duplicates allowed?
+   ignore blanks, but allow duplicates
+ codemaker picks pattern (e.g. 4 colors and their position)
+ for number of games
+   while less than specified number of turns (12)...
+     codebreaker picks a pattern
+     codemaker provides feedback
+            ● - correct position and color
+            ◑ - correct color, wrong position
+            ◌ - incorrect color and position
+   tally points - codemaker gets 1 point for each guess/row utilized by codebreaker
+   switch roles
+ user with most points is declared winner
+ colors: 0 1 2 3 4 -
+ example
+ codemaker: 0 1 2 3
+ codebreaker:
+ 1| 0 2 3 4 | ● ◑ ◑ ◌
+ 2| 0 2 3 1 | ● ◑ ◑ ◑
+ 3| 0 1 3 2 | ● ● ◑ ◑
+ 4| 0 1 2 3 | ● ● ● ●
+ Secret number: 4034
+ Every round there are A bagels and 1 picos.
+ [12 digit: 0] 1. 0000  1A01  (so there's one 0)
+ [12 digit: 1] 2. 0111  0A11  (so there's no 1's, and the 0 is in the wrong place)
+ [12 digit: 2] 3. 2022  1A01  (so there's no 2's either, but we know where the 0 is now)
+ [12 digit: 3] 4. 3033  2A01  (so there's one 3, because A+1 increased by 1)
+ [12 digit: 4] 5. 3044  2A21  (you now have all the numbers, just not in the right order)
+ [12 digit: -] 6. 4043  2A21  (the switch didn't work, try another one)
+ [12 digit: -] 7. 4034  4A01  (you win!)
+ puts '1 ... 0 2 3 4 | ● ◑ ◑ ◌'
--- mastermind.rb
+++ mastermind.rb
@@ -1,44 +1,3 @@
-# decide number of turns (e.g. 12)?
-# decide codebreaker vs codemaker?
-# decide how many games? (1 game means computer and player swap roles)
-# decide if blanks or duplicates allowed?
-#   ignore blanks, but allow duplicates
-# codemaker picks pattern (e.g. 4 colors and their position)
-
-# for number of games
-#   while less than specified number of turns (12)...
-#     codebreaker picks a pattern
-#     codemaker provides feedback
-#            ● - correct position and color
-#            ◑ - correct color, wrong position
-#            ◌ - incorrect color and position
-#   tally points - codemaker gets 1 point for each guess/row utilized by codebreaker
-#   switch roles
-# user with most points is declared winner
-
-# colors: 0 1 2 3 4 -
-
-# example
-# codemaker: 0 1 2 3
-# codebreaker:
-# 1| 0 2 3 4 | ● ◑ ◑ ◌
-# 2| 0 2 3 1 | ● ◑ ◑ ◑
-# 3| 0 1 3 2 | ● ● ◑ ◑
-# 4| 0 1 2 3 | ● ● ● ●
-
-# Secret number: 4034
-# Every round there are A bagels and 1 picos.
-
-# [12 digit: 0] 1. 0000  1A01  (so there's one 0)
-# [12 digit: 1] 2. 0111  0A11  (so there's no 1's, and the 0 is in the wrong place)
-# [12 digit: 2] 3. 2022  1A01  (so there's no 2's either, but we know where the 0 is now)
-# [12 digit: 3] 4. 3033  2A01  (so there's one 3, because A+1 increased by 1)
-# [12 digit: 4] 5. 3044  2A21  (you now have all the numbers, just not in the right order)
-# [12 digit: -] 6. 4043  2A21  (the switch didn't work, try another one)
-# [12 digit: -] 7. 4034  4A01  (you win!)
-
-# puts '1 ... 0 2 3 4 | ● ◑ ◑ ◌'
-
 module Intro
   ODIN = "         AN
 ░█▀█░█▀▄░▀█▀░█▀█░░░█▀█░█▀▄░█▀█░█▀▄░█░█░█▀▀░▀█▀░▀█▀░█▀█░█▀█░█▀▀
@@ -153,11 +112,16 @@ feedback = initialize_hash(NUM_GUESSES, NUM_SLOTS)
 
 j = 0
 while j < NUM_GUESSES && !pattern_solved(feedback)
-  puts 'Make a guess'
-  guesses[j] = user_pattern(NUM_SLOTS, NUM_CHOICES)
+  puts "\nCODEBREAKER: Make a guess."
+
+  pattern = user_pattern(NUM_SLOTS, NUM_CHOICES)
+  until validate_user_pattern(pattern, NUM_SLOTS)
+    puts "\nInvalid input: #{pattern.join}"
+    pattern = user_pattern(NUM_SLOTS, NUM_CHOICES)
+  end
+  guesses[j] = pattern
   feedback[j] = give_feedback(rand_patt, guesses[j])
 
-  # preview the table
   render_codebreaker_table(guesses, feedback, NUM_GUESSES, NUM_SLOTS)
   j += 1
 end
